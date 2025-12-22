fedfbc7f9d24fedc0f7ef09f705ef0857b06487d
linktrich <eugene4guy@gmail.com>
2025-12-22 23:07:44 +0300

    Initial commit

---
+++ src/api/article/routes/custom-article.ts
@@ -0,0 +1,21 @@
+﻿export default {
+    routes: [
+        {
+            method: 'POST',
+            path: '/articles/:id/publish',
+            handler: 'api::article.article.publish',
+            config: {
+                policies: [],
+            },
+        },
+        {
+            method: 'GET',
+            path: '/articles/:id/author',
+            handler: 'api::article.article.getAuthor',
+            config: {
+                policies: [],
+                middlewares: [],
+            },
+        },
+    ]
+}
---
+++ src/api/user-role/controllers/user-role.ts
@@ -0,0 +1,21 @@
+export default {
+    async getMyRole(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized();
+        }
+
+        const fullUser = await strapi
+            .query('plugin::users-permissions.user')
+            .findOne({
+                where: { id: user.id },
+                populate: ['role'],
+            });
+
+        ctx.body = {
+            roleId: fullUser.role.id,
+            roleName: fullUser.role.name,
+        };
+    },
+};
